c80d501c8ec6e384d99b0783c0cbd8368f46a979
Leesin0222 <dragon02229@naver.com>
2022-03-13 15:04:29 +0900

    📂 :: 파일구조 변경

--- GitChart/presentation/src/main/java/com/yongjincompany/gitchart/ui/MainActivity.kt
+++ GitChart/presentation/src/main/java/com/yongjincompany/gitchart/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yongjincompany.gitchart
+package com.yongjincompany.gitchart.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yongjincompany.gitchart.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
