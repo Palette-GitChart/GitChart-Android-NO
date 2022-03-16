7ba250a8f787921df0b474009091c17fa5a411c9
Leesin0222 <dragon02229@naver.com>
2022-03-16 20:14:52 +0900

    ⚡ :: 연간커밋 RoomEntity 작성

--- data/src/main/java/com/yongjincompany/data/local/entity/YearCommitRoomEntity.kt
+++ data/src/main/java/com/yongjincompany/data/local/entity/YearCommitRoomEntity.kt
@@ -1,8 +1,21 @@
 package com.yongjincompany.data.local.entity
 
 import androidx.room.Entity
+import com.yongjincompany.domain.entity.home.YearCommitEntity
 
 @Entity(tableName = "yearCommit")
 data class YearCommitRoomEntity(
     val yearCommit: Int
 )
+
+fun YearCommitRoomEntity.toEntity() =
+    YearCommitEntity(
+        yearCommit = yearCommit
+    )
+
+fun YearCommitEntity.toDbEntity() =
+    YearCommitRoomEntity(
+        yearCommit = yearCommit
+    )
+
+
