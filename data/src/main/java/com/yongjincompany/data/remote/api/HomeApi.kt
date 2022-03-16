53dba0358227475df6e696899882b7fd4bf7f6ce
Leesin0222 <dragon02229@naver.com>
2022-03-16 10:10:32 +0900

    ♻️ :: Myprofile 로 함수명 변경

--- data/src/main/java/com/yongjincompany/data/remote/api/HomeApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/HomeApi.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Path
 
 interface HomeApi {
     @GET("users/{userId}")
-    suspend fun fetchGithubProfile(
+    suspend fun fetchMyProfile(
         @Path("userId") userId: String
     ): MyProfileResponse
 
