f5da29a65c5ab2bf73aab0cb70e8154ebbb0ee14
Leesin0222 <dragon02229@naver.com>
2022-03-16 20:14:38 +0900

    ⚡ :: 주간커밋RoomEntity 작성

--- data/src/main/java/com/yongjincompany/data/local/entity/WeeklyCommitRoomEntity.kt
+++ data/src/main/java/com/yongjincompany/data/local/entity/WeeklyCommitRoomEntity.kt
@@ -1,8 +1,19 @@
 package com.yongjincompany.data.local.entity
 
 import androidx.room.Entity
+import com.yongjincompany.domain.entity.home.WeeklyCommitEntity
 
 @Entity(tableName = "weekCommit")
 data class WeeklyCommitRoomEntity(
     val weekCommit: Int
 )
+
+fun WeeklyCommitRoomEntity.toEntity() =
+    WeeklyCommitEntity(
+        weekCommit = weekCommit
+    )
+
+fun WeeklyCommitEntity.toDbEntity() =
+    WeeklyCommitRoomEntity(
+        weekCommit = weekCommit
+    )
